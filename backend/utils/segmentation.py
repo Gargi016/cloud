eccc27cc8a3284967121dba0e2fa9c22adda83b5
Gargi Das <egargidas@gmail.com>
2025-12-15 02:16:35 +0530

    Add cloud segmentation function using OpenCV

--- backend/utils/segmentation.py
+++ backend/utils/segmentation.py
@@ -1,1 +1,14 @@
+import cv2
+
+def segment_clouds(img):
+    blur = cv2.GaussianBlur(img, (5, 5), 0)
+    mask = cv2.adaptiveThreshold(
+        blur,
+        255,
+        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv2.THRESH_BINARY,
+        11,
+        2
+    )
+    return mask
 
